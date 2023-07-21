1a4ec8f9bb3822ebab2f2a4166de42573ba7fec4
Charles Ludwig <charleswludwig@gmail.com>
2023-07-21 13:55:45 -0400

    updating api for countries json reponse from db

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 import os, psycopg2
 from database import load_records_from_db
+import pandas as pd
 
 load_dotenv()
 
 app = Flask(__name__)
 
 connection_records = load_records_from_db()
-
-CAT_LOC = []
-
-for record in connection_records:
-    CAT_LOC.append({
-        'country': record[0],
-        'latitude': record[1],
-        'longitude': record[2],
-        'name': record[3],
-})
 
 fact_url = 'https://catfact.ninja/fact'
 
@@ -27,22 +18,32 @@ url_search = f"https://api.thecatapi.com/v1/breeds?api_key={cat_api_key}"
 
 @app.route("/")
 def index():
-    return render_template('index.html', brython_cat_breeds = url_search, brython_fact_url = fact_url,
-    brython_table_db = CAT_LOC
-)
-
-@app.route("/cat_map", methods=["POST"])
-def form_submit(): 
-    cat_code_data = request.form.get("cat_code")
-    
-    print(cat_code_data)
-
+    """
     for i in CAT_LOC:
         if str(cat_code_data) in i['country']:
             url_lat_lon_list = list(i['latitude'], i['longitude'])
-  
-    return render_template('cat_map.html', brython_cat_breeds = url_search, brython_fact_url = fact_url,
-    url_lat_lon = url_lat_lon_list)
+
+            for i in {{ brython_table_db }}:
+                if cat_facts_dict["country_code"] in i['country']:
+                    lat, long, country, name = i['latitude'], i['longitude'], i['country'], i['name']
+
+    """
+    return render_template('index.html', brython_cat_breeds = url_search, brython_fact_url = fact_url
+)
+
+@app.route("/api/country", methods=["GET"])
+def get_all_users():
+    if connection_records:
+        result = []
+        for record in connection_records:
+            result.append({
+                'country': record[0],
+                'latitude': record[1],
+                'longitude': record[2],
+                'name': record[3]})        
+        return jsonify(result)
+    else:
+        return jsonify({"error": f"country not found."}), 404          
 
 if __name__ == '__main__':
     app.run()
